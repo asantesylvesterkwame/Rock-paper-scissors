7c9cbe736f980381b4e71dfaf043bb64a6468156
asantesylvesterkwame <intelligentson17@gmail.com>
2022-11-26 01:02:53 +0000

    setup the logic for the buttons

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 
-    let computerPossibleChoices = [1, 2, 3]
-    let randomComputerChoices = Math.floor(Math.random() * 3) + 1 ;
-    function getComputerChoice(){
-    if (randomComputerChoices === computerPossibleChoices[1]){
-        return 'rock' ;
-    }else if (randomComputerChoices === computerPossibleChoices[2]){
-        return 'paper';
-    }else {
-        return 'scissors';
-    }
-   }
-   
+
    
   
-    let playerScoreCounter = 0
+   /* let playerScoreCounter = 0
    let compScoreCounter = 0
 
    function playRound(playerSelection, computerSelection){
@@ -62,9 +51,9 @@
 
    }
 
- console.log(playRound(document.getElementById(''), getComputerChoice()))
+ console.log(playRound(userRock, getComputerChoice()))
 
-   /*let numberOfRounds = ""
+   let numberOfRounds = ""
     function game(){
     for (let i = 1; i < 5; i++){
      numberOfRounds +=playRound(prompt('please enter your WEAPON, LET\'S FIGHT'), getComputerChoice());
@@ -82,4 +71,135 @@
         
     console.log(game());*/
     
-   
+   let userRock = document.getElementById('firstBox');
+   let userPaper = document.getElementById('secondBox');
+   let userScissors = document.getElementById('thirdBox');
+   let userScore = document.getElementById('playerScore');
+   let compScore = document.getElementById('computerScore');
+   let playerScoreCounter = 0
+   let compScoreCounter = 0
+    userRock.addEventListener('click', function () {
+        let computerPossibleChoices = [1, 2, 3]
+        let randomComputerChoices = Math.floor(Math.random() * 3) + 1 ;
+        function getComputerChoice(){
+        if (randomComputerChoices === computerPossibleChoices[1]){
+            return 'rock' ;
+        }else if (randomComputerChoices === computerPossibleChoices[2]){
+            return 'paper';
+        }else {
+            return 'scissors';
+        }
+       }
+        console.log(getComputerChoice()); 
+        
+        let rockBtnChoice = 'rock';
+        console.log(rockBtnChoice);
+
+       let ComputerRandomChoice = getComputerChoice();
+    console.log(ComputerRandomChoice)
+
+       
+       if (getComputerChoice() === rockBtnChoice) {
+        playerScoreCounter += 0;
+        userScore.innerHTML = playerScoreCounter;
+        compScoreCounter += 0;
+        compScore.innerHTML = compScoreCounter;
+       }
+       if (getComputerChoice() === 'paper') {
+        if (rockBtnChoice === 'rock') {
+            compScoreCounter += 1;
+            compScore.innerHTML = compScoreCounter;            
+        }
+      }
+       if (getComputerChoice() === 'scissors') {
+        if (rockBtnChoice === 'rock') {
+            playerScoreCounter += 1;
+            userScore.innerHTML = playerScoreCounter;            
+        }
+       }
+
+    });
+
+    userPaper.addEventListener('click', function () {
+        let computerPossibleChoices = [1, 2, 3]
+        let randomComputerChoices = Math.floor(Math.random() * 3) + 1 ;
+        function getComputerChoice(){
+        if (randomComputerChoices === computerPossibleChoices[1]){
+            return 'rock' ;
+        }else if (randomComputerChoices === computerPossibleChoices[2]){
+            return 'paper';
+        }else {
+            return 'scissors';
+        }
+       }
+        console.log(getComputerChoice()); 
+        
+        let paperBtnChoice = 'paper';
+        console.log(paperBtnChoice);
+
+       let ComputerRandomChoice = getComputerChoice();
+    console.log(ComputerRandomChoice)
+
+       
+       if (getComputerChoice() === paperBtnChoice) {
+        playerScoreCounter += 0;
+        userScore.innerHTML = playerScoreCounter;
+        compScoreCounter += 0;
+        compScore.innerHTML = compScoreCounter;
+       }
+       if (getComputerChoice() === 'rock') {
+        if (paperBtnChoice === 'paper') {
+            playerScoreCounter += 1;
+            userScore.innerHTML = playerScoreCounter;            
+        }
+      }
+       if (getComputerChoice() === 'scissors') {
+        if (paperBtnChoice === 'paper') {
+            compScoreCounter += 1;
+            compScore.innerHTML = compScoreCounter;            
+        }
+       }
+
+    });
+    
+    userScissors.addEventListener('click', function () {
+        let computerPossibleChoices = [1, 2, 3]
+        let randomComputerChoices = Math.floor(Math.random() * 3) + 1 ;
+        function getComputerChoice(){
+        if (randomComputerChoices === computerPossibleChoices[1]){
+            return 'rock' ;
+        }else if (randomComputerChoices === computerPossibleChoices[2]){
+            return 'paper';
+        }else {
+            return 'scissors';
+        }
+       }
+        console.log(getComputerChoice()); 
+        
+        let scissorsBtnChoice = 'scissors';
+        console.log(scissorsBtnChoice);
+
+       let ComputerRandomChoice = getComputerChoice();
+    console.log(ComputerRandomChoice)
+
+       
+       if (getComputerChoice() === scissorsBtnChoice) {
+        playerScoreCounter += 0;
+        userScore.innerHTML = playerScoreCounter;
+        compScoreCounter += 0;
+        compScore.innerHTML = compScoreCounter;
+       }
+       if (getComputerChoice() === 'paper') {
+        if (scissorsBtnChoice === 'scissors') {
+            playerScoreCounter += 1;
+            userScore.innerHTML = playerScoreCounter;            
+        }
+      }
+       if (getComputerChoice() === 'rock') {
+        if (scissorsBtnChoice === 'scissors') {
+            compScoreCounter += 1;
+            compScore.innerHTML = compScoreCounter;            
+        }
+       }
+
+    });
